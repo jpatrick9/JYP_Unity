5ba01c4c567bcf61075b2debe09f85e4127fcda6
Joshua Patrick <jpatrick@csumb.edu>
2019-01-06 13:44:36 -0800

    Basic Point-and-Shooter
    2D clickable game with working score system and win condition

---
+++ 2DShooter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject target;
+    int score = 0;
+    public Text scoreText;
+    bool win = false;
+    public GameObject winText;
+
+    public AudioSource[] winSound = new AudioSource[2];
+    
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", 1.0f, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (win == true)
+        {
+            CancelInvoke("Spawn");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //GetComponent<AudioSource>().Play();
+            winSound[0].Play();
+        }
+
+	}
+
+    void Spawn()
+    {
+        float randomX;
+        float randomY;
+
+        randomX = Random.Range(-17f, 17f);
+        randomY = Random.Range(-9.5f, 9.5f);
+
+        Vector3 randomPosition = new Vector3(randomX, randomY, 0);
+
+        Instantiate(target, randomPosition, Quaternion.identity);
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+        print(score);
+        scoreText.text = score.ToString();
+
+        if (score >= 5)
+        {
+            win = true;
+            winText.SetActive(true);
+            winSound[1].Play();
+        }
+    }
+
+}
---
+++ 2DShooter/Assets/Scripts/Target.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    public GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        Destroy(gameObject, 2.0f);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnMouseDown()
+    {
+        gameManager.IncrementScore();
+
+        Destroy(gameObject);
+    }
+
+}
